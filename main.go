0841f888a982c955805edc6dd3c2ed11f91b0ade
Wes McNamee <wes.mcnamee+github@outreach.io>
2018-02-15 10:00:53 -0800

    very simple rest-chat implementation

---
+++ main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"github.com/gorilla/sessions"
+	"strings"
+)
+
+var store = sessions.NewCookieStore([]byte("something-very-secret"))
+
+var things []string
+
+func getSession(w http.ResponseWriter, r *http.Request) (*sessions.Session) {
+	session, err := store.Get(r, "session-name")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return nil
+	}
+
+	return session
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("view handler!")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	for _, thing := range things {
+		fmt.Fprintf(w, "%v<br />", thing)
+	}
+}
+
+func sayHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("say handler!")
+	session := getSession(w, r)
+	if session == nil {
+		return
+	}
+
+	name := session.Values["name"]
+	if name == "" || name == nil {
+		name = "Anon"
+	}
+
+	parts := strings.Split(r.URL.Path, "/")
+	thingSaid := strings.Join(parts[2:], " ")
+	if thingSaid != "" {
+		things = append(things, fmt.Sprintf("%v: %v", name, thingSaid))
+	}
+
+	viewHandler(w, r)
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("login handler!")
+	session := getSession(w, r)
+	if session == nil {
+		return
+	}
+
+	parts := strings.Split(r.URL.Path, "/")
+	name := parts[2]
+
+	session.Values["name"] = name
+	session.Save(r, w)
+
+	fmt.Fprintf(w, "Logged you in, %v!", name)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	parts := strings.Split(r.URL.Path, "/")
+	base := parts[1]
+
+	fmt.Printf("%v -> %v\n", r.URL.Path, base)
+
+	if strings.ToLower(base) == "login" {
+		loginHandler(w, r)
+	} else if strings.ToLower(base) == "say" {
+		sayHandler(w, r)
+	} else {
+		viewHandler(w, r)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
